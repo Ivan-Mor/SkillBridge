525d0054e97ea5064d079e15c888990e91f659fe
Evgeni <thehacjlednik@gmail.com>
2026-07-18 17:15:18 +0300

    fixed imports

--- config/urls.py
+++ config/urls.py
@@ -15,10 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from apps.common.views import HealthCheckView
 from django.contrib import admin
 from django.urls import path
-
-from common.views import HealthCheckView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
